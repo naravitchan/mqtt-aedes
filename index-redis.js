14298dfc77ad524a6ab3012ae8152e01315cb070
naravich <naravich@enres.co>
2020-03-19 16:35:16 +0700

    redis const

--- index-redis.js
+++ index-redis.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 var redis = require('mqemitter-redis')
 var aedesPersistenceRedis = require('aedes-persistence-redis');
 
+// set mq
 var mq = redis({
     port: process.env.REDIS_PORT,   // Redis port
     host: process.env.REDIS_HOST,   // Redis host
@@ -9,6 +10,7 @@ var mq = redis({
     password: process.env.REDIS_PASSWORD,
 })
 
+// set persistence
 var persistence = aedesPersistenceRedis({
     port: process.env.REDIS_PORT,   // Redis port
     host: process.env.REDIS_HOST,   // Redis host
@@ -21,16 +23,49 @@ var persistence = aedesPersistenceRedis({
     }
 })
 
-console.log(process.env.REDIS_PORT);
-
 var aedes = require('aedes')({
     mq,
     persistence
 })
 
-var server = require('net').createServer(aedes.handle)
+//  authenticate user / password
+aedes.authenticate = function (client, username, password, callback) {
+    var authorized = (username == 'mqtt' && password.toString() == 'password');
+    if (authorized) client.user = username;
+    callback(null, authorized);
+}
 
+// create server
+var server = require('net').createServer(aedes.handle)
 server.listen(1883, function () {
     console.log('start Mqtt with Redis Persistance!')
-    // aedes.publish({ topic: 'aedes/hello', payload: "I'm broker " + aedes.id })
+    aedes.publish({ topic: 'aedes/hello', payload: "I'm broker " + aedes.id })
+})
+
+aedes.on('clientError', function (client, err) {
+    console.log('client error', client.id, err.message, err.stack)
+})
+
+aedes.on('connectionError', function (client, err) {
+    console.log('client error', client, err.message, err.stack)
+})
+
+aedes.on('publish', function (packet, client) {
+    if (client) {
+        console.log('message from client', client.id)
+    }
+    switch (packet.topic) {
+        case 'test':
+            return console.log('test get : ' + packet.payload.toString())
+    }
+})
+
+aedes.on('subscribe', function (subscriptions, client) {
+    if (client) {
+        console.log('subscribe from client', subscriptions, client.id)
+    }
+})
+
+aedes.on('client', function (client) {
+    console.log('new client', client.id)
 })
